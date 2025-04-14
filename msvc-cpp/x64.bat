0ab98155f99703e88ec302ae420437b1b77e7ca4
Nicholas <nicholas@fitzroydale.org>
2025-04-14 14:32:03 +1000

    Update paths for new versions of tools

--- msvc-cpp/x64.bat
+++ msvc-cpp/x64.bat
@@ -6,11 +6,13 @@ set CONAN_NON_INTERACTIVE=1
 
 set CMAKE_GENERATOR=Ninja
 
+REM C:\Tools\VS2022\VC\Tools\MSVC\\14.43.34808\bin\HostX64\x64
+
 set ExtensionSdkDir=C:\Tools\SDK\10\ExtensionSDKs
-set INCLUDE=C:\Tools\VS2022\VC\Tools\MSVC\14.38.33130\include;C:\Tools\VS2022\VC\Tools\MSVC\14.38.33130\ATLMFC\include;C:\Tools\VS2022\VC\Auxiliary\VS\include;C:\Tools\SDK\10\include\10.0.20348.0\ucrt;C:\Tools\SDK\10\include\10.0.20348.0\um;C:\Tools\SDK\10\include\10.0.20348.0\shared;C:\Tools\SDK\10\include\10.0.20348.0\\winrt;C:\Tools\SDK\10\include\10.0.20348.0\cppwinrt
-set LIB=C:\Tools\VS2022\VC\Tools\MSVC\14.38.33130\ATLMFC\lib\x64;C:\Tools\VS2022\VC\Tools\MSVC\14.38.33130\lib\x64;C:\Tools\SDK\10\lib\10.0.20348.0\ucrt\x64;C:\Tools\SDK\10\\lib\10.0.20348.0\um\x64
-set LIBPATH=C:\Tools\VS2022\VC\Tools\MSVC\14.38.33130\ATLMFC\lib\x64;C:\Tools\VS2022\VC\Tools\MSVC\14.38.33130\lib\x64;C:\Tools\VS2022\VC\Tools\MSVC\14.38.33130\lib\x64\store\references;C:\Tools\SDK\10\UnionMetadata\10.0.20348.0;C:\Tools\SDK\10\References\10.0.20348.0
-set PATH=C:\Tools\Git\cmd;C:\Tools\Conan\conan;C:\Tools\CMake\bin;C:\Tools\Wix;C:\Tools\Ninja;C:\Tools\VS2022\VC\Tools\MSVC\14.38.33130\bin\HostX64\x64;C:\Tools\SDK\10\bin\10.0.20348.0\x64;C:\Tools\SDK\10\bin\x64;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;
+set INCLUDE=C:\Tools\VS2022\VC\Tools\MSVC\14.43.34808\include;C:\Tools\VS2022\VC\Tools\MSVC\14.43.34808\ATLMFC\include;C:\Tools\VS2022\VC\Auxiliary\VS\include;C:\Tools\SDK\10\include\10.0.20348.0\ucrt;C:\Tools\SDK\10\include\10.0.20348.0\um;C:\Tools\SDK\10\include\10.0.20348.0\shared;C:\Tools\SDK\10\include\10.0.20348.0\\winrt;C:\Tools\SDK\10\include\10.0.20348.0\cppwinrt
+set LIB=C:\Tools\VS2022\VC\Tools\MSVC\14.43.34808\ATLMFC\lib\x64;C:\Tools\VS2022\VC\Tools\MSVC\14.43.34808\lib\x64;C:\Tools\SDK\10\lib\10.0.20348.0\ucrt\x64;C:\Tools\SDK\10\\lib\10.0.20348.0\um\x64
+set LIBPATH=C:\Tools\VS2022\VC\Tools\MSVC\14.43.34808\ATLMFC\lib\x64;C:\Tools\VS2022\VC\Tools\MSVC\14.43.34808\lib\x64;C:\Tools\VS2022\VC\Tools\MSVC\14.43.34808\lib\x64\store\references;C:\Tools\SDK\10\UnionMetadata\10.0.20348.0;C:\Tools\SDK\10\References\10.0.20348.0
+set PATH=C:\Tools\Git\cmd;C:\Tools\Conan\conan;C:\Tools\CMake\bin;C:\Tools\Wix;C:\Tools\Ninja;C:\Tools\VS2022\VC\Tools\MSVC\14.43.34808\bin\HostX64\x64;C:\Tools\SDK\10\bin\10.0.20348.0\x64;C:\Tools\SDK\10\bin\x64;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;
 set PATHEXT=.COM;.EXE;.BAT;.CMD;.VBS;.VBE;.JS;.JSE;.WSF;.WSH;.MSC
 set Platform=x64
 set PROCESSOR_ARCHITECTURE=AMD64
@@ -18,9 +20,9 @@ set UCRTVersion=10.0.20348.0
 set UniversalCRTSdkDir=C:\Tools\SDK\10\
 set VS170COMNTOOLS=C:\Tools\Common7\Tools\
 set VCINSTALLDIR=C:\Tools\VS2022\VC\
-set VCToolsInstallDir=C:\Tools\VS2022\VC\Tools\MSVC\14.38.33130\
-set VCToolsRedistDir=C:\Tools\VS2022\VC\Redist\MSVC\14.38.33130\
-set VCToolsVersion=14.38.33130
+set VCToolsInstallDir=C:\Tools\VS2022\VC\Tools\MSVC\14.43.34808\
+set VCToolsRedistDir=C:\Tools\VS2022\VC\Redist\MSVC\14.43.34808\
+set VCToolsVersion=14.43.34808
 set VisualStudioVersion=17.0
 set VSCMD_ARG_app_plat=Desktop
 set VSCMD_ARG_HOST_ARCH=x64
